acfc906b652c9e8d450490150a6db5aa69f45f32
Bryan <bryan.heshiki@gmail.com>
2024-08-13 18:17:42 -0700

    revise top nav and hamburger menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,53 @@
-console.log("Hello!");
+const btnOpen = document.getElementById("btnOpen");
+const btnClose = document.getElementById("btnClose");
+const breakpoint = window.matchMedia("(width < 43.75em)");
+const menuTopNav = document.getElementById("menuTopNav");
+const overlay = document.getElementById("overlay");
+
+const main = document.getElementById("main");
+const footer = document.getElementById("footer");
+
+const setupTopNav = () => {
+  if (breakpoint.matches) {
+    menuTopNav.setAttribute("inert", "");
+  } else {
+    menuTopNav.removeAttribute("inert");
+  }
+};
+
+setupTopNav();
+
+const openMobileMenu = () => {
+  btnOpen.setAttribute("aria-expanded", "true");
+  main.setAttribute("inert", "");
+  menuTopNav.removeAttribute("inert");
+  footer.setAttribute("inert", "");
+
+  menuTopNav.style.transitionDuration = "0.4s";
+  overlay.style.transitionDuration = "0.4s";
+  bodyScrollLock.disableBodyScroll(menuTopNav);
+  btnClose.focus();
+};
+
+const closeMobileMenu = () => {
+  btnOpen.setAttribute("aria-expanded", "false");
+  main.removeAttribute("inert");
+  footer.removeAttribute("inert");
+  menuTopNav.setAttribute("inert", "");
+
+  bodyScrollLock.enableBodyScroll(menuTopNav);
+  btnOpen.focus();
+
+  setTimeout(() => {
+    menuTopNav.removeAttribute("style");
+    overlay.removeAttribute("style");
+  }, 500);
+};
+
+btnOpen.addEventListener("click", openMobileMenu);
+btnClose.addEventListener("click", closeMobileMenu);
+
+breakpoint.addEventListener("change", () => {
+  console.log("breakpoint");
+  setupTopNav();
+});
